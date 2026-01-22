b25e97fb641bb7f6253311396c888e80a6570a04
faithank15 <oswaldkagbahinto@gmail.com>
2026-01-22 14:58:00 +0100

    Initialisation

---
+++ src/groupe6/Groupe6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package groupe6;
+
+/**
+ *
+ * @author faithan15
+ */
+public class Groupe6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
